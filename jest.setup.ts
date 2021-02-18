9283296564852ea025730001aac0c8ded8987bc4
Noe Francisco de Oliveira Neto <noe.de.oliveira.neto@avanade.com>
2021-02-18 13:50:33 -0300

    fix: config setup jest travis CI

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,3 +3,10 @@ dotenv.config({ path: "./.env.test" });
 
 import "reflect-metadata";
 import "@infra/IoC/RootInjectionDependency";
+import tsConfigPaths from "tsconfig-paths";
+import tsConfig from "./tsconfig.json";
+
+tsConfigPaths.register({
+  baseUrl: tsConfig.compilerOptions.baseUrl,
+  paths: tsConfig.compilerOptions.paths,
+});
